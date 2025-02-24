1dc81543136bf2ef8b4986302c0fb86b921aa73d
Emilianno <emidudickel@gmail.com>
2025-02-24 10:56:51 -0300

    Criado modelagem entre Usuario e Permissao

---
+++ src/main/java/Entity/Permissoes.java
@@ -0,0 +1,27 @@
+package Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_permissoes")
+public class Permissoes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String permissao;
+
+    @OneToMany (mappedBy = "permissoes")//1 permissao para varios usuarios e mapear ela na tabela usuario.
+    private List<Usuario> usuario;
+}
--- src/main/java/Entity/Usuario.java
+++ src/main/java/Entity/Usuario.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity //transforma uma classe em entity no Banco de Dados
 @Getter
 @Setter
@@ -17,7 +19,15 @@ public class Usuario {
     private Long id;
 
     private String nome;
+
     private String email;
+
     private int idade;
+
+    @ManyToOne //quero que na tabela do usuario, tenha uma unica permissao
+    @JoinColumn (name = "permissao_id") //juntar as duas colunas com chave estrangeira
+    private Permissoes permissoes;
+
+
 
 }
