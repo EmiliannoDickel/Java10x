2e4a870b6f59aacc0e3f12af2a464abdbdb81943
Emilianno <emidudickel@gmail.com>
2025-02-24 09:15:27 -0300

    primeiro commit

---
+++ src/test/java/dev/java10x/cadastrobasico/CadastroBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.cadastrobasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
