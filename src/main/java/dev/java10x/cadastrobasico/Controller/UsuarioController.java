5e9a12324d8ababc6de6e6ded70d589de6c13809
Emilianno <emidudickel@gmail.com>
2025-02-27 14:53:53 -0300

    feat: adicionado UsuarioController com as CRUDs e APIs com o Postman em teste

--- src/main/java/dev/java10x/cadastrobasico/Controller/UsuarioController.java
+++ src/main/java/dev/java10x/cadastrobasico/Controller/UsuarioController.java
@@ -1,4 +1,56 @@
 package dev.java10x.cadastrobasico.Controller;
 
+import dev.java10x.cadastrobasico.Entity.Usuario;
+import dev.java10x.cadastrobasico.Service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuario")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    //Pegar Lista De Usuarios
+    @GetMapping("/all")
+    public List<Usuario> pegarUsuarios () {
+        return usuarioService.pegarUsuarios();
+    }
+
+    //Pegar pelo ID
+    @GetMapping("/{id}")
+    public Usuario pegarUsuarioId (@PathVariable Long id) {
+        return usuarioService.pegarUsuarioId(id);
+    }
+
+    //Criar Usuario
+    @PostMapping("/new")
+    public Usuario criarUsuario (@RequestBody Usuario usuario) {
+        return usuarioService.criarUsuario(usuario);
+    }
+
+    //Deletar Todos Usuarios
+    @DeleteMapping("/all")
+    public void excluirTodosUsuario () {
+         usuarioService.excluirTodosUsuario();
+    }
+
+    //Deletar Usuario por ID
+    @DeleteMapping("/{id}")
+    public void removerUsuarioId (@PathVariable Long id) {
+        usuarioService.excluirUsuario(id);
+    }
+
+    @PutMapping("/alterar/{id}")
+    public Usuario alterarUsuario (@PathVariable Long id, @RequestBody Usuario usuarioAtualizado ) {
+        return usuarioService.atualizarUsuario(id, usuarioAtualizado);
+    }
+
+
+
+
+
 }
