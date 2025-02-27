d73a7659cdb4eb67886dde1ea9c68b7218b1b0e7
Emilianno <emidudickel@gmail.com>
2025-02-27 14:34:06 -0300

    feat: adicionado UsuarioService e as funcionalidades CRUD

--- src/main/java/dev/java10x/cadastrobasico/Service/UsuarioService.java
+++ src/main/java/dev/java10x/cadastrobasico/Service/UsuarioService.java
@@ -1,4 +1,51 @@
 package dev.java10x.cadastrobasico.Service;
 
+import dev.java10x.cadastrobasico.Entity.Usuario;
+import dev.java10x.cadastrobasico.Repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    //Pegar Todos Os Usuarios
+    public List<Usuario> pegarUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    //Pegar Usuario por ID
+    public Usuario pegarUsuarioId(Long id) {
+        Optional<Usuario> usuarioId = usuarioRepository.findById(id);
+        return usuarioId.orElse(null); //ou me mostra o usuario, ou passa null
+    }
+
+    //Criar Usuario
+    public Usuario criarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    //Deletar Todos Usuarios
+    public void excluirTodosUsuario() {
+        usuarioRepository.deleteAll();
+    }
+
+    //Deletar Usuario Por ID
+    public void excluirUsuario(Long Id){
+        usuarioRepository.deleteById(Id);
+    }
+
+    //Alterar Usuario
+    public Usuario atualizarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+
+
+
 }
