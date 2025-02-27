006618b5ecea1550be1449abb4ddd788a7237b07
Emilianno <emidudickel@gmail.com>
2025-02-27 14:37:50 -0300

    feat: adicionado PermissaoService e suas requisições CRUD

--- src/main/java/dev/java10x/cadastrobasico/Service/PermissoesService.java
+++ src/main/java/dev/java10x/cadastrobasico/Service/PermissoesService.java
@@ -1,4 +1,48 @@
 package dev.java10x.cadastrobasico.Service;
 
+import dev.java10x.cadastrobasico.Entity.Permissoes;
+import dev.java10x.cadastrobasico.Repository.PermissoesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
 public class PermissoesService {
+    @Autowired
+    private PermissoesRepository permissoesRepository;
+
+    //Pegar Permissoes
+    public List<Permissoes> pegarPermissao() {
+        return permissoesRepository.findAll();
+    }
+
+    //Pegar Permissao por ID
+    public Permissoes pegarPermissaoId(Long id) {
+        Optional<Permissoes> permissaoId = permissoesRepository.findById(id);
+        return permissaoId.orElse(null); //ou me mostra a permissao, ou passa null
+    }
+
+    //Criar Permissao
+    public Permissoes criarPermissao(Permissoes permissoes) {
+        return permissoesRepository.save(permissoes);
+    }
+
+    //Deletar Todas Permissoes
+    public void excluirTodasPermissoes() {
+        permissoesRepository.deleteAll();
+    }
+
+    //Deletar permissao Por ID
+    public void excluirPermissaoId(Long Id){
+        permissoesRepository.deleteById(Id);
+    }
+
+    //Alterar permissao
+    public Permissoes atualizarPermissao(Permissoes permissoes) {
+        return permissoesRepository.save(permissoes);
+    }
+
+
+
 }
