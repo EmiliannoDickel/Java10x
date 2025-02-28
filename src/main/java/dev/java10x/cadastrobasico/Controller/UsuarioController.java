7f5ba820fca6fcd669e880b24d7cfd73262b3104
Emilianno <emidudickel@gmail.com>
2025-02-28 16:38:08 -0300

    refactor: refatora o Controller custumizando com o ResponseEntity as APIs

--- src/main/java/dev/java10x/cadastrobasico/Controller/UsuarioController.java
+++ src/main/java/dev/java10x/cadastrobasico/Controller/UsuarioController.java
@@ -4,6 +4,9 @@ import dev.java10x.cadastrobasico.Entity.Usuario;
 import dev.java10x.cadastrobasico.Entity.UsuarioDTO;
 import dev.java10x.cadastrobasico.Service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,20 +20,28 @@ public class UsuarioController {
 
     //Pegar Lista De Usuarios
     @GetMapping("/all")
-    public List<UsuarioDTO> pegarUsuarios () {
-        return usuarioService.pegarUsuarios();
+    public ResponseEntity<List<UsuarioDTO>> pegarUsuarios () {
+        List<UsuarioDTO> usuariosLista = usuarioService.pegarUsuarios();
+        return ResponseEntity.ok(usuariosLista);
     }
 
     //Pegar pelo ID
     @GetMapping("/{id}")
-    public UsuarioDTO pegarUsuarioId (@PathVariable Long id) {
-        return usuarioService.pegarUsuarioId(id);
+    public ResponseEntity<?> pegarUsuarioId (@PathVariable Long id) { //ponto de pergunta faz com que o usuario possa ser devolvido em JSON
+       UsuarioDTO usuario = usuarioService.pegarUsuarioId(id);
+       if (usuario != null) {
+           return ResponseEntity.ok(usuario);
+       } else {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário: " + id + " não Encontrado!");
+       }
     }
 
     //Criar Usuario
     @PostMapping("/new")
-    public UsuarioDTO criarUsuario (@RequestBody UsuarioDTO usuarioDTO) {
-        return usuarioService.criarUsuario(usuarioDTO);
+    public ResponseEntity<String> criarUsuario (@RequestBody UsuarioDTO usuario) {
+        UsuarioDTO novoUsuario = usuarioService.criarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Usuário Criado! Seja Bem Vindo(a) " + novoUsuario.getNome() + " (ID): " + novoUsuario.getId()) ;
     }
 
     //Deletar Todos Usuarios
@@ -41,13 +52,26 @@ public class UsuarioController {
 
     //Deletar Usuario por ID
     @DeleteMapping("/{id}")
-    public void removerUsuarioId (@PathVariable Long id) {
-        usuarioService.excluirUsuario(id);
+    public ResponseEntity<String> excluirUsuario (@PathVariable Long id) {
+        if (usuarioService.pegarUsuarioId(id) != null) {
+            usuarioService.excluirUsuario(id);
+            return ResponseEntity.ok("Usuário: " + id + " Excluido!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário Não Encontrado!");
+        }
     }
 
     @PutMapping("/alterar/{id}")
-    public UsuarioDTO alterarUsuario (@PathVariable Long id, @RequestBody UsuarioDTO usuarioAtualizado ) {
-        return usuarioService.atualizarUsuario(id, usuarioAtualizado);
+    public ResponseEntity<String> alterarUsuario (@PathVariable Long id, @RequestBody UsuarioDTO usuarioAtualizado ) {
+        if (usuarioService.pegarUsuarioId(id) != null) {
+            usuarioService.atualizarUsuario(id, usuarioAtualizado);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body("Usuário: " + usuarioAtualizado.getNome() + " Alterado!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário Não Encontrado!");
+        }
+
+
     }
 
 
