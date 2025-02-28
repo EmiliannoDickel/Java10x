e83cf6e90d02163e5a113c4ad18c138e1a643f53
Emilianno <emidudickel@gmail.com>
2025-02-28 09:56:17 -0300

    chore: inclui DTO e Mapper do usuario

--- src/main/java/dev/java10x/cadastrobasico/Entity/UsuarioDTO.java
+++ src/main/java/dev/java10x/cadastrobasico/Entity/UsuarioDTO.java
@@ -1,4 +1,64 @@
 package dev.java10x.cadastrobasico.Entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
+        private Long id;
+        private String nome;
+        private String email;
+        private int idade;
+        private Permissoes permissoes;
+        private String senha;
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Permissoes getPermissoes() {
+        return permissoes;
+    }
+
+    public void setPermissoes(Permissoes permissoes) {
+        this.permissoes = permissoes;
+    }
 }
