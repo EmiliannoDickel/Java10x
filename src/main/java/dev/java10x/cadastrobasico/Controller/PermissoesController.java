cc86171c12222522dccccd6390fa5278434a35f1
Emilianno <emidudickel@gmail.com>
2025-03-06 16:19:23 -0300

    doc: adiciona o Swagger na minha aplicação para documentar o usuarioController e permissaoController

--- src/main/java/dev/java10x/cadastrobasico/Controller/PermissoesController.java
+++ src/main/java/dev/java10x/cadastrobasico/Controller/PermissoesController.java
@@ -2,6 +2,10 @@ package dev.java10x.cadastrobasico.Controller;
 
 import dev.java10x.cadastrobasico.Entity.Permissoes;
 import dev.java10x.cadastrobasico.Service.PermissoesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +19,13 @@ public class PermissoesController {
     @Autowired
     private PermissoesService permissoesService;
 
-    //Pegar Lista De Usuarios
+    //Pegar Lista De Permissao
     @GetMapping("/all")
+    @Operation (summary = "Pegar lista com todos as Permissões", description = "essa rota pega todos as Permissões do meu banco de dados em lista.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista Encontrado com Sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Lista não encontrada")
+    })
     public ResponseEntity<List<Permissoes>> pegarPermissao () {
         List<Permissoes> permissoesList = permissoesService.pegarPermissoes();
         return ResponseEntity.ok(permissoesList);
@@ -24,6 +33,11 @@ public class PermissoesController {
 
     //Pegar pelo ID
     @GetMapping("/{id}")
+    @Operation (summary = "Pegar pelo ID", description = "pega as Permissão no banco pelo ID que for colocado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Permissão Encontrada"),
+            @ApiResponse(responseCode = "400", description = "Permissão não encontrada")
+    })
     public ResponseEntity<?> pegarPermissaoId (@PathVariable Long id) {
         Permissoes permissoes = permissoesService.pegarPermissaoId(id);
         if (permissoes != null) {
@@ -34,21 +48,36 @@ public class PermissoesController {
 
     }
 
-    //Criar Usuario
+    //Criar Permissao
     @PostMapping("/new")
+    @Operation (summary = "Criar Permissão", description = "Cria uma Permissão no meu banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Permissão Criada Com Sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na Criação da Permissão")
+    })
     public ResponseEntity<String> criarPermissao (@RequestBody Permissoes permissoes) {
         Permissoes permissoesNova = permissoesService.criarPermissao(permissoes);
         return ResponseEntity.status(HttpStatus.CREATED).body("Permissão Criada! Nova Função: " + permissoesNova.getNome() +" adicionada ao sistema.");
     }
 
-    //Deletar Todos Usuarios
+    //Deletar Todos Permissao
     @DeleteMapping("/all")
+    @Operation (summary = "Deletar todas Permissões", description = "Deleta todas Permissões no meu banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Deletadas Com Sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro no deletar todas as Permissões")
+    })
     public void excluirTodasPermissoes () {
         permissoesService.excluirTodasPermissoes();
     }
 
-    //Deletar Usuario por ID
+    //Deletar Permissao por ID
     @DeleteMapping("/{id}")
+    @Operation (summary = "Deleta uma Permissão pelo ID", description = "Deleta a Permissão pelo ID no meu banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Permissão Deletada Com Sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro no Deletar a Permissão")
+    })
     public ResponseEntity<String> excluirPermissaoId (@PathVariable Long id) {
         if (permissoesService.pegarPermissaoId(id) != null) {
             permissoesService.excluirPermissaoId(id);
@@ -59,7 +88,17 @@ public class PermissoesController {
     }
 
     @PutMapping("/alterar/{id}")
-    public ResponseEntity<String> atualizarPermissao (@PathVariable Long id, @RequestBody Permissoes permissaoAtualizada ) {
+    @Operation (summary = "Alterar Permissão", description = "Altera a Permissão no meu banco de dados com uso do ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Permissão Alterada Com Sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro no Alterar a Permissão")
+    })
+    public ResponseEntity<String> atualizarPermissao(
+            @Parameter(description = "Usuário manda o ID no caminho da requisição") //documenta os parametros
+            @PathVariable Long id,
+            @Parameter(description = "Usuário manda os dados da Permissão Atualizado no corpo da requisição")
+            @RequestBody Permissoes permissaoAtualizada ) {
+
         if (permissoesService.pegarPermissaoId(id) != null) {
             permissoesService.atualizarPermissao(id, permissaoAtualizada);
             return ResponseEntity.status(HttpStatus.CREATED)
--- src/main/java/dev/java10x/cadastrobasico/Controller/UsuarioController.java
+++ src/main/java/dev/java10x/cadastrobasico/Controller/UsuarioController.java
@@ -3,6 +3,9 @@ package dev.java10x.cadastrobasico.Controller;
 import dev.java10x.cadastrobasico.Entity.Usuario;
 import dev.java10x.cadastrobasico.Entity.UsuarioDTO;
 import dev.java10x.cadastrobasico.Service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -24,6 +27,11 @@ public class UsuarioController {
 
     //Pegar Lista De Usuarios
     @GetMapping("/all")
+    @Operation(summary = "Pegar lista com todos os usuários", description = "essa rota pega todos os usuários do meu banco de dados em lista.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista Encontrado com Sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Lista não encontrada")
+    })
     public ResponseEntity<List<UsuarioDTO>> pegarUsuarios () {
         List<UsuarioDTO> usuariosLista = usuarioService.pegarUsuarios();
         return ResponseEntity.ok(usuariosLista);
@@ -31,6 +39,11 @@ public class UsuarioController {
 
     //Pegar pelo ID
     @GetMapping("/{id}")
+    @Operation (summary = "Pegar pelo ID", description = "pega o usuário no banco pelo ID que for colocado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário Encontrado"),
+            @ApiResponse(responseCode = "400", description = "Usuário não encontrado")
+    })
     public ResponseEntity<?> pegarUsuarioId (@PathVariable Long id) { //ponto de pergunta faz com que o usuario possa ser devolvido em JSON
        UsuarioDTO usuario = usuarioService.pegarUsuarioId(id);
        if (usuario != null) {
@@ -42,6 +55,11 @@ public class UsuarioController {
 
     //Criar Usuario
     @PostMapping("/new")
+    @Operation (summary = "Criar Usuário", description = "Cria um usuário no meu banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usúário Criado Com Sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na Criação do Usuário")
+    })
     public ResponseEntity<String> criarUsuario (@RequestBody UsuarioDTO usuario) {
         UsuarioDTO novoUsuario = usuarioService.criarUsuario(usuario);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -50,12 +68,22 @@ public class UsuarioController {
 
     //Deletar Todos Usuarios
     @DeleteMapping("/all")
+    @Operation (summary = "Deletar todos Usuários", description = "Deleta todos usuário no meu banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Deletados Com Sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro no deletar todos os Usuários")
+    })
     public void excluirTodosUsuario () {
          usuarioService.excluirTodosUsuario();
     }
 
     //Deletar Usuario por ID
     @DeleteMapping("/{id}")
+    @Operation (summary = "Deleta um Usuário pelo ID", description = "Deleta o usuário pelo ID no meu banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usúário Deletado Com Sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro no Deletar o Usuário")
+    })
     public ResponseEntity<String> excluirUsuario (@PathVariable Long id) {
         if (usuarioService.pegarUsuarioId(id) != null) {
             usuarioService.excluirUsuario(id);
@@ -66,6 +94,11 @@ public class UsuarioController {
     }
 
     @PutMapping("/alterar/{id}")
+    @Operation (summary = "Alterar Usuário", description = "Altera o usuário no meu banco de dados com uso do ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usúário Alterado Com Sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro no Alterar o Usuário")
+    })
     public ResponseEntity<String> alterarUsuario (@PathVariable Long id, @RequestBody UsuarioDTO usuarioAtualizado ) {
         if (usuarioService.pegarUsuarioId(id) != null) {
             usuarioService.atualizarUsuario(id, usuarioAtualizado);
